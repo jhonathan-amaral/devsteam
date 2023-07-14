53b96d8a73cc2d5e148c87627437c7f3ea6374f5
Jhonathan <you@example.com>
2023-07-14 19:55:27 -0300

    create game card

--- src/components/cards/saleCard/saleCard.jsx
+++ src/components/cards/saleCard/saleCard.jsx
@@ -15,7 +15,7 @@ export default function SaleCard(){
                         <h4 className={styles.discountprice}>R$89,90</h4>
                     </div>
                 </div>
-                <Button>Adcionar ao carrinho</Button>
+                <Button fullWidth>Adcionar ao carrinho</Button>
             </div>
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
+import GameCard from "@/components/cards/gameCard/gameCard";
 import SaleCard from "@/components/cards/saleCard/saleCard";
 import Container from "@/components/container/container";
 import Navbar from "@/components/navbar/navbar";
 import Subtitle from "@/components/tipography/subtitle/subititle";
+import styles from "@/styles/index.module.css";
 import Head from "next/head";
 
 export default function Home() {
@@ -18,14 +20,23 @@ export default function Home() {
       </Head>
       <Navbar />
       <Container>
-        <div>
+        <div className={styles.session}>
           <Subtitle>Promoçoes</Subtitle>
-          <div>
+          <div className={styles.salecontainer}>
+            <SaleCard />
+            <SaleCard />
             <SaleCard />
           </div>
         </div>
-        <div>
+        <div className={styles.session}>
           <Subtitle>Outros jogos</Subtitle>
+          <div className={styles.gamecontainer}>
+            <GameCard />
+            <GameCard />
+            <GameCard />
+            <GameCard />
+            <GameCard />
+          </div>
         </div>
       </Container>
     </>
